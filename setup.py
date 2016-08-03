a5dd31979a00a8727c6cc041ef2186d3c4509878
Bernardas <bernardas.alisauskas@gmail.com>
2016-08-03 05:42:52 +0000

    minor hotfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gosuticker',
-    version='0.2',
+    version='0.21',
     packages=['gosuticker'],
     url='https://github.com/Granitas/gosuticker',
     license='GPLV3+',
