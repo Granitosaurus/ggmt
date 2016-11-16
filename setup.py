c52a556757880dd6ab485975107e6725ac0c11b5
Bernardas <bernardas.alisauskas@gmail.com>
2016-11-16 01:29:56 +0000

    increment version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gosuticker',
-    version='0.21',
+    version='0.3',
     packages=['gosuticker'],
     url='https://github.com/Granitas/gosuticker',
     license='GPLV3+',
