b58c309d5b5c047fbca48ddfcf3048508f7d8f30
Bernardas <bernardas.alisauskas@gmail.com>
2016-11-17 15:28:07 +0000

    rework keys to use ordered dictionary with descriptions

--- gosuticker/__init__.py
+++ gosuticker/__init__.py
@@ -1,18 +1,22 @@
+from collections import OrderedDict
+
+
 class Match(dict):
     keys = [
-        'url',
-        'id',
-        'game',
-        'time',
-        'time_secs',
-        't1',
-        't1_country',
-        't1_country_short',
-        't2',
-        't2_country',
-        't2_country_short',
-        'stream',
+        ('url', 'url to gosugamers match page'),
+        ('id', 'gosugamers match id'),
+        ('game', 'game name'),
+        ('time', 'time in text as displayed on gosugamers'),
+        ('time_secs', 'time in integer seconds'),
+        ('t1', 'name of team 1'),
+        ('t1_country', 'country of team 1'),
+        ('t1_country_short', 'short version of country of team 1'),
+        ('t2', 'cname of team 2'),
+        ('t2_country', 'country of team 2'),
+        ('t2_country_short', 'short version of country of team 2'),
+        ('stream', 'direct stream url to match hosting channel'),
     ]
+    keys = OrderedDict(keys)
 
     def __setitem__(self, key, value):
         if key in self.keys:
