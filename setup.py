cf51f19285927b10592c9b8a6372c4fe1ac0860d
Bernardas <bernardas.alisauskas@gmail.com>
2017-04-06 17:52:49 +0000

    bump as per minor bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ggmt',
-    version='0.9.1',
+    version='0.9.2',
     packages=['ggmt'],
     url='https://github.com/Granitosaurus/ggmt',
     license='GPLV3+',
